744f6e2c15b478402030db77c7b3c3d92aadd531
misha <mikhail.dyoshin@gmail.com>
2023-12-27 16:19:58 +0300

    Succeded to display a list of amplitudes on display using Canvas.

--- app/src/main/java/com/example/mediadiaryproject/presentation/audiorecordingscreen/AudioRecordingScreen.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/audiorecordingscreen/AudioRecordingScreen.kt
@@ -4,10 +4,17 @@ package com.example.mediadiaryproject.presentation.audiorecordingscreen
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
@@ -29,6 +36,8 @@ fun AudioRecordingScreen(
         ).show()
     }
 
+    val rectList = listOf(100, 200, 300)
+
     Column {
         Text(text = "Audio recording screen")
         if (recording) {
@@ -47,5 +56,24 @@ fun AudioRecordingScreen(
             Text(text = "Stop")
         }
         Text(text = "$amplitude")
+        androidx.compose.foundation.Canvas(
+            modifier = Modifier.width(200.dp).height(100.dp),
+            onDraw = {
+                val canvasWidth = size.width
+                val canvasHeight = size.height
+                val spaceBetween = 10
+
+                for (height in rectList.withIndex()) {
+                    val xOffset = spaceBetween + height.index * (10 + spaceBetween)
+                    val yOffset = canvasHeight/2 - height.value/2
+                    drawRect(
+                        color = Color.Gray,
+                        topLeft = Offset(x = xOffset.toFloat(), y = yOffset),
+                        size = Size(width = 10F, height = height.value.toFloat())
+                    )
+                }
+
+            }
+        )
     }
 }
