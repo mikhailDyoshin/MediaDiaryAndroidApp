f713d97739c5998b6f310c04ba9844e5ff0c4708
misha <mikhail.dyoshin@gmail.com>
2024-01-22 16:29:04 +0300

    Refactored the TextNoteEditScreen
    Deleted a view-model and a navigater from the composable.

--- app/src/main/java/com/example/mediadiaryproject/presentation/textnoteeditscreen/TextNoteEditScreen.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/textnoteeditscreen/TextNoteEditScreen.kt
@@ -3,6 +3,8 @@ package com.example.mediadiaryproject.presentation.textnoteeditscreen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -10,6 +12,8 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mediadiaryproject.presentation.destinations.TextNotesScreenDestination
 import com.example.mediadiaryproject.presentation.textnoteeditscreen.components.NoteTextField
@@ -18,59 +22,73 @@ import com.example.mediadiaryproject.presentation.textnoteeditscreen.viewmodel.T
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Destination
 @Composable
 fun TextNoteEditScreen(
+    noteTitle: String,
+    noteText: String,
     textNoteToEditId: Int = -1,
-    navigator: DestinationsNavigator,
-    viewModel: TextNoteEditScreenViewModel = hiltViewModel(),
     dayId: Int,
+    setNoteData: (noteId: Int) -> Unit,
+    updateDayId: (dayId: Int) -> Unit,
+    updateTitle: (title: String) -> Unit,
+    updateNoteText: (text: String) -> Unit,
+    updateNote: (noteId: Int) -> Unit,
+    saveNote: () -> Unit,
+    navigateToNotes: (dayId: Int) -> Unit,
 ) {
 
     if (textNoteToEditId != -1) {
         LaunchedEffect(true) {
-            viewModel.setNoteData(textNoteToEditId)
+            setNoteData(textNoteToEditId)
         }
     } else {
-        viewModel.updateDayId(dayId)
+        updateDayId(dayId)
     }
 
-    Column {
-        Text(text = "Text note screen")
-        Text(text = viewModel.editedTextNoteDate)
+    Column( modifier = Modifier.padding(horizontal = 10.dp, vertical = 8.dp) ) {
         TitleTextField(
-            title = viewModel.title,
-            updateTitle = { title -> viewModel.updateTitle(title) }
+            title = noteTitle,
+            updateTitle = { title -> updateTitle(title) }
         )
-//        TextField(
-//            value = viewModel.text,
-////            modifier = Modifier.fillMaxHeight(),
-//            onValueChange = { text -> viewModel.updateText(text) },
-//            maxLines = 5
-//        )
         NoteTextField(
-            note = viewModel.text,
-            updateNote = { value -> viewModel.updateText(value) },
+            note = noteText,
+            updateNote = { value -> updateNoteText(value) },
             modifier = Modifier
         )
-        Row() {
-            if (textNoteToEditId != -1) {
-                Button(onClick = {
-                    viewModel.updateNote(textNoteToEditId)
-                }) {
-                    Text(text = "Edit")
-                }
-            } else {
-                Button(onClick = { viewModel.saveNote() }) {
-                    Text(text = "Save")
-                }
+
+        Row(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+            Button(onClick = {
+                if (textNoteToEditId != -1) {
+                    updateNote(textNoteToEditId)
+                } else saveNote()
+            }) {
+                Text(text = "Save")
             }
-            Button(onClick = { navigator.navigate(TextNotesScreenDestination(dayId = dayId)) }) {
-                Text(text = "Today's notes")
+
+            Button(onClick = { navigateToNotes(dayId) }) {
+                Text(text = "Notes")
             }
         }
 
     }
 
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun TextNoteEditScreenPreview() {
+    TextNoteEditScreen(
+        noteTitle = "My note",
+        noteText = "Note text text text text text text text text text text text text text text text ",
+        textNoteToEditId = 1,
+        dayId = 1,
+        setNoteData = {},
+        updateDayId = {},
+        updateTitle = {},
+        updateNoteText = {},
+        updateNote = {},
+        saveNote = {},
+        navigateToNotes = {}
+    )
 }
