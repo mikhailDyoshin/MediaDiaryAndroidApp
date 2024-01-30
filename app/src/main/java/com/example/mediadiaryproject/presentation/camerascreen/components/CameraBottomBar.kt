bf2a8392d61c42e0784a71c1f7e72117d5158f91
misha <mikhail.dyoshin@gmail.com>
2024-01-30 10:35:05 +0300

    Added a logic to change camera-mode-buttons' colors in the bottom-bar of the CameraContent composable.

--- app/src/main/java/com/example/mediadiaryproject/presentation/camerascreen/components/CameraBottomBar.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/camerascreen/components/CameraBottomBar.kt
@@ -10,14 +10,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mediadiaryproject.R
+import com.example.mediadiaryproject.ui.theme.HalfTransparent
 
 @Composable
 fun CameraBottomBar(
@@ -38,15 +41,29 @@ fun CameraBottomBar(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Button(onClick = { capturePhoto() }) {
+            Button(
+                onClick = { capturePhoto() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (videoMode) Color.Transparent else HalfTransparent,
+                    contentColor = Color.White
+                )
+            ) {
                 Text("Photo")
             }
-            Button(onClick = { recordVideo() }) {
+            Button(
+                onClick = { recordVideo() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (videoMode) HalfTransparent else Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
                 Text("Video")
             }
         }
         Row(
-            modifier = Modifier.fillMaxWidth().padding(top = 20.dp, bottom = 20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 20.dp),
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically,
         ) {
@@ -64,7 +81,7 @@ fun CameraBottomBar(
 
 @Preview
 @Composable
-fun CameraBottomBarPreview() {
+fun CameraBottomBarVideoModePreview() {
 
     val context = LocalContext.current
 
@@ -73,6 +90,24 @@ fun CameraBottomBarPreview() {
 
     CameraBottomBar(
         videoMode = true,
+        lastCapturedPhoto = image,
+        capturePhoto = {},
+        recordVideo = {},
+        toggleCamera = {},
+        displayLastPhoto = {})
+}
+
+@Preview
+@Composable
+fun CameraBottomBarPhotoModePreview() {
+
+    val context = LocalContext.current
+
+    val imageId = R.drawable.placeholder_image
+    val image = BitmapFactory.decodeResource(context.resources, imageId)
+
+    CameraBottomBar(
+        videoMode = false,
         lastCapturedPhoto = image,
         capturePhoto = {},
         recordVideo = {},
