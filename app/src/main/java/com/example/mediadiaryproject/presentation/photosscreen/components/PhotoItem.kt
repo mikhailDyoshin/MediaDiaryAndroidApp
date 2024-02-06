7d5d2f53ae1f33c73378c24762c872b31133b9a8
misha <mikhail.dyoshin@gmail.com>
2024-02-06 16:10:37 +0300

    Customized the list of photos

--- app/src/main/java/com/example/mediadiaryproject/presentation/photosscreen/components/PhotoItem.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/photosscreen/components/PhotoItem.kt
@@ -2,16 +2,29 @@ package com.example.mediadiaryproject.presentation.photosscreen.components
 
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mediadiaryproject.R
 import com.example.mediadiaryproject.presentation.photosscreen.state.PhotoState
 
@@ -20,18 +33,47 @@ fun PhotoItem(photo: PhotoState) {
 
     val image = photo.image
 
-    Column {
-        if (image != null) {
-            Image(
-                bitmap = image.asImageBitmap(),
-                contentDescription = null,
+    val cornerSize = 15.dp
+
+    Column(modifier = Modifier
+        .padding(top = 10.dp)
+        .clickable { }) {
+        Row(modifier = Modifier.shadow(5.dp, shape = RoundedCornerShape(cornerSize), clip = true)) {
+            Row(
                 modifier = Modifier
-                    .height(100.dp)
-                    .width(100.dp)
-            )
+                    .fillMaxWidth()
+                    .background(color = Color.White, shape = RoundedCornerShape(cornerSize))
+                    .padding(vertical = 10.dp, horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                if (image != null) {
+                    Image(
+                        bitmap = image.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(100.dp)
+                            .width(100.dp)
+                            .clip(shape = RoundedCornerShape(size = 15.dp))
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 10.dp)
+                ) {
+                    Text(
+                        photo.title,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(photo.description)
+                }
+            }
         }
-        Text(photo.title)
-        Text(photo.description)
+
+
     }
 }
 
--- app/src/main/java/com/example/mediadiaryproject/presentation/photosscreen/components/PhotosListScreen.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/photosscreen/components/PhotosListScreen.kt
@@ -1,18 +1,58 @@
 package com.example.mediadiaryproject.presentation.photosscreen.components
 
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mediadiaryproject.R
 import com.example.mediadiaryproject.presentation.photosscreen.state.PhotoState
 
 @Composable
 fun PhotosListScreen(listOfPhotos: List<PhotoState>) {
 
-    Column {
-        Column {
-            listOfPhotos.forEach { photo ->
-                PhotoItem(photo = photo)
-            }
+    Column(modifier = Modifier
+        .padding(horizontal = 10.dp)
+        .fillMaxHeight()) {
+        listOfPhotos.forEach { photo ->
+            PhotoItem(photo = photo)
         }
-
     }
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PhotosListScreenPreview() {
+
+    val context = LocalContext.current
+
+    val imageId = R.drawable.placeholder_image
+    val image = BitmapFactory.decodeResource(context.resources, imageId)
+
+    val listOfPhotos = listOf(
+        PhotoState(
+            title = "My photo 1",
+            description = "This is a photo of my...",
+            image = image
+        ),
+        PhotoState(
+            title = "My photo long long long title",
+            description = "This is a photo of my...",
+            image = image
+        ),
+        PhotoState(
+            title = "My photo 3",
+            description = "This is a photo of my...",
+            image = image
+        )
+
+
+    )
+
+    PhotosListScreen(listOfPhotos = listOfPhotos)
 }
