1f47a85cec2b98e89792fd034b5f43fa29ba75b7
misha <mikhail.dyoshin@gmail.com>
2024-02-07 09:40:37 +0300

    Created a new screen for photo viewing.
    Just get started to create a new screen.

---
+++ app/src/main/java/com/example/mediadiaryproject/presentation/photoview/components/PhotoViewCore.kt
@@ -0,0 +1,38 @@
+package com.example.mediadiaryproject.presentation.photoview.components
+
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mediadiaryproject.R
+import com.example.mediadiaryproject.presentation.photoview.state.PhotoViewState
+
+@Composable
+fun PhotoViewCore(photoState: PhotoViewState) {
+    Box() {
+
+            Image(
+                bitmap = photoState.image.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PhotoViewCorePreview() {
+    val context = LocalContext.current
+
+    val imageId = R.drawable.placeholder_image
+    val image = BitmapFactory.decodeResource(context.resources, imageId)
+
+    PhotoViewCore(photoState = PhotoViewState(title = "", description = "", image = image))
+}
---
+++ app/src/main/java/com/example/mediadiaryproject/presentation/photoview/state/PhotoViewState.kt
@@ -0,0 +1,5 @@
+package com.example.mediadiaryproject.presentation.photoview.state
+
+import android.graphics.Bitmap
+
+data class PhotoViewState(val title: String, val description: String, val image: Bitmap)
