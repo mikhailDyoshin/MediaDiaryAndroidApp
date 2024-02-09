1c03c454c4e7b76c2e53d810df76e84c2e8b3519
misha <mikhail.dyoshin@gmail.com>
2024-02-09 11:11:53 +0300

    Created a carousel for next-notes.

--- app/src/main/java/com/example/mediadiaryproject/presentation/carousel/Carousel.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/carousel/Carousel.kt
@@ -7,11 +7,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -25,10 +25,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
+import com.example.mediadiaryproject.presentation.daycontentscreen.components.TextNoteCard
+import com.example.mediadiaryproject.presentation.daycontentscreen.state.TextNoteCardState
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.calculateCurrentOffsetForPage
@@ -45,7 +49,13 @@ fun <T> Carousel(data: List<T>, cardContent: @Composable (data: T) -> Unit) {
 
     val scope = rememberCoroutineScope()
 
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .background(color = Color.White)
+            .padding(bottom = 5.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
 
         Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
 
@@ -65,29 +75,37 @@ fun <T> Carousel(data: List<T>, cardContent: @Composable (data: T) -> Unit) {
                 state = pagerState,
                 contentPadding = PaddingValues(horizontal = 65.dp),
                 modifier = Modifier
-                    .height(350.dp)
+                    .height(110.dp)
                     .weight(1f)
             ) { page ->
                 val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                val shadowElevation = if (page == pagerState.currentPage) 4.dp else 0.dp
+                Column(
+                    modifier = Modifier.shadow(
+                        shadowElevation,
+                        shape = RoundedCornerShape(10.dp),
+                        clip = true
+                    )
+                ) {
 
-                Card(shape = RoundedCornerShape(10.dp), modifier = Modifier
-                    .graphicsLayer {
-                        lerp(
-                            start = 0.75f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        ).also { scale ->
-                            scaleX = scale
-                            scaleY = scale
-                        }
-                        alpha = lerp(
-                            start = 0.5f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        )
-                    }) {
-                    cardContent(data[page])
-
+                    Card(shape = RoundedCornerShape(10.dp), modifier = Modifier
+                        .graphicsLayer {
+                            lerp(
+                                start = 0.75f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            )
+                        }) {
+                        cardContent(data[page])
+                    }
                 }
             }
 
@@ -105,8 +123,8 @@ fun <T> Carousel(data: List<T>, cardContent: @Composable (data: T) -> Unit) {
         }
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
+                .fillMaxWidth(),
+//                .height(50.dp),
             horizontalArrangement = Arrangement.Center
         ) {
             repeat(data.size) {
@@ -128,4 +146,32 @@ fun <T> Carousel(data: List<T>, cardContent: @Composable (data: T) -> Unit) {
         }
 
     }
+}
+
+@Preview
+@Composable
+fun CarouselPreview() {
+
+    val data = listOf(
+        TextNoteCardState(
+            id = 0,
+            title = "Weather is about to change",
+            text = "Weather is very good"
+        ),
+        TextNoteCardState(
+            id = 1,
+            title = "Weather is about to change",
+            text = "Weather is very good"
+        ),
+        TextNoteCardState(
+            id = 2,
+            title = "Weather is about to change",
+            text = "Weather is very good"
+        ),
+    )
+
+    Carousel(data) { dataUnit ->
+        TextNoteCard(state = dataUnit)
+    }
+
 }
--- app/src/main/java/com/example/mediadiaryproject/presentation/daycontentscreen/components/TextNoteCard.kt
+++ app/src/main/java/com/example/mediadiaryproject/presentation/daycontentscreen/components/TextNoteCard.kt
@@ -4,12 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -22,11 +25,13 @@ import com.example.mediadiaryproject.presentation.daycontentscreen.state.TextNot
 @Composable
 fun TextNoteCard(state: TextNoteCardState) {
 
+    val cornerSize = 10.dp
+
 
     Column(
         modifier = Modifier
             .background(color = Color.White)
-            .height(100.dp)
+            .heightIn(min = 80.dp, max = 100.dp)
             .width(200.dp)
     ) {
         Row(verticalAlignment = Alignment.Top) {
@@ -53,6 +58,7 @@ fun TextNoteCard(state: TextNoteCardState) {
             }
         }
     }
+
 }
 
 
